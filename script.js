e6d01abff6314ab742d2b850437cecb6bc79c1cb
Lukortech <lukortech@wp.pl>
2022-05-18 14:17:10 +0200

    feat(dropdown): menu now hides and reacts to resize event

--- script.js
+++ script.js
@@ -1,42 +1,25 @@
 "use strict"
 
-const icon = document.getElementById("icon");
-const options = document.getElementById("options");
-const hamburger = document.getElementsByClassName("bar");
+const icon = document.querySelector("#menu-icon");
+const dropdown = document.querySelector("#dropdown");
 
+const bars = document.querySelectorAll(".bar");
 
-icon.addEventListener("click", ()=>{
+const showDropdownMenu = (shouldShow) => {
+  if (!shouldShow) return dropdown.classList.add("hidden");
+  dropdown.classList.toggle("hidden");
+}
 
-    options.classList.toggle("dropdown");
-    options.classList.toggle("hidden");
-    hamburger[0].classList.toggle("ab1");
-    hamburger[1].classList.toggle("ab2");
+const animateBurger = () => {
+  bars.classList.toggle("borgir");
+}
 
-
-    //icon.classList.toggle("hidden");
-    //icon.classList.toggle("flex");
+// Listeners
+icon.addEventListener("click", () => {
+  showDropdownMenu(true);
+  animateBurger();
 });
 
-/*
-const debounce = (callback, delay) => {
-    let timer;
-
-    return function (...args) {
-        if (timer) clearTimeout(timer);
-
-        timer = setTimeout(() => {
-            callback(...args);
-        }, delay);
-    };
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    const debouncedResize = debounce(function (event) {
-
-        options.classList.toggle("hidden");
-        icon.classList.toggle("hidden");
-    }, 500);
-
-    input.addEventListener("input", debouncedResize);
-});*/
+window.addEventListener("resize", () => {
+  return showDropdownMenu(false)
+})
