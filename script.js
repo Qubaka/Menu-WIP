0fb35ca53e05628b7c338bc877e3ab2d0140f0b6
Qubaka <kozikuba204@gmail.com>
2022-05-19 20:13:59 +0200

    working debounce

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
 "use strict"
 
-const icon = document.querySelector("#menu-icon");
-const dropdown = document.querySelector("#dropdown");
-
-const bars = document.querySelectorAll(".bar");
-
-const showDropdownMenu = (shouldShow) => {
-  if (!shouldShow) return dropdown.classList.add("hidden");
-  dropdown.classList.toggle("hidden");
+//Utils/Helpers
+// const debounce = (callback, delay) => {
+//     let timer;
+  
+//     return function (...args) {
+//       if (timer) clearTimeout(timer);
+  
+//       timer = setTimeout(() => {
+//         callback(...args);
+//       }, delay);
+//     };
+// };
+function debounce(func, time){
+    var time = time || 100; // 100 by default if no param
+    var timer;
+    return function(event){
+        if(timer) clearTimeout(timer);
+        timer = setTimeout(func, time, event);
+    };
 }
 
+const icon = document.querySelector("#menu-icon");
+const dropdown = document.querySelector("#dropdown");
+const background = document.querySelector("#blur");
+const bars = document.querySelectorAll(".bar");
+const bod = document.querySelector(".bod");
+
+function showDropdownMenu(shouldShow){
+  if (!shouldShow){
+      dropdown.classList.add("hidden");
+      background.classList.add("hidden");
+  }else{
+    dropdown.classList.toggle("hidden");
+    background.classList.toggle("hidden");
+  }
+}
+
+const debounceShow = debounce(showDropdownMenu,500);
+
+
 const animateBurger = () => {
-  bars.classList.toggle("borgir");
+  bars[0].classList.toggle("borgir1");
+  bars[1].classList.toggle("borgir2");
+  bars[2].classList.toggle("borgir3");
 }
 
 // Listeners
+background.addEventListener("click", () => {
+  showDropdownMenu(true);
+  animateBurger();
+})
 icon.addEventListener("click", () => {
   showDropdownMenu(true);
   animateBurger();
 });
-
-window.addEventListener("resize", () => {
-  return showDropdownMenu(false)
+window.addEventListener("resize", function(){
+  debounceShow(false);
 })
